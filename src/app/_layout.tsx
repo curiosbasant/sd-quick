6a62d6a43c9e3b588efe0b2b59816b2f4037e834
CuriosBasant <basantbrpl@gmail.com>
2025-02-07 12:00:25 +0530

    chore: ➕ add `nativewind`

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,18 @@
+import 'react-native-reanimated'
+import '../global.css'
+
 import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
 import { StatusBar } from 'expo-status-bar'
 import { useEffect } from 'react'
-import 'react-native-reanimated'
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
   const [loaded] = useFonts({
-    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require('~/assets/fonts/SpaceMono-Regular.ttf'),
   })
 
   useEffect(() => {
@@ -25,10 +27,10 @@ export default function RootLayout() {
 
   return (
     <>
+      <StatusBar style='auto' />
       <Stack>
         <Stack.Screen name='+not-found' />
       </Stack>
-      <StatusBar style='auto' />
     </>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require('nativewind/preset')],
+  content: ['src/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
