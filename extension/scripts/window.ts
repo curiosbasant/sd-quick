3ea88ed49298d8b58e989fb731a663e0f79f4888
CuriosBasant <basantbrpl@gmail.com>
2025-02-23 22:14:22 +0530

    feat: ✨ set dynamic page title

--- extension/scripts/window.ts
+++ extension/scripts/window.ts
@@ -10,5 +10,17 @@ window.timeInSecondsAfterSessionOut = Infinity
 
 // refresh every 5 minutes to avoid session timeout
 setInterval(() => {
-  fetch(`${location.origin + location.pathname.slice(0, 4)}/Home/School/Home_New.aspx`)
+  const sdNum = location.pathname.slice(0, 4) // /SD{1-4}
+  fetch(`${location.origin + sdNum}/Home/School/Home_New.aspx`)
 }, 5 * 60 * 1000)
+
+// Set the window title
+document.title = (() => {
+  const title =
+    // Get the title from the active breadcrumb or from the page title
+    document.querySelector('ol.breadcrumb li.active, .box_heading h2')?.textContent?.trim() ??
+    // Get the last pathname segment
+    location.pathname.split('/').at(-1)?.slice(0, -5)
+
+  return title ? `${title} | ShalaDarpan` : 'ShalaDarpan'
+})()
