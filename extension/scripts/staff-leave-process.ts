849627824db0ff9bed5cae25fa09018448c62f6c
CuriosBasant <basantbrpl@gmail.com>
2025-02-27 23:17:04 +0530

    feat: ✨ trigger checkbox if status changed for `staff-leave-process`

---
+++ extension/scripts/staff-leave-process.ts
@@ -0,0 +1,20 @@
+import { repeatUntil } from '../utils'
+
+repeatUntil((done) => {
+  const trs = document.querySelectorAll<HTMLTableRowElement>(
+    '#ContentPlaceHolder1_grdLeaveDetails tr:has(td)'
+  )
+  if (!trs.length) return
+
+  trs.forEach((tr) => {
+    const checkbox = tr.children[0].firstElementChild as HTMLInputElement
+    const select = tr.children[8].firstElementChild as HTMLSelectElement
+    select.addEventListener('change', () => {
+      // Trigger the checkbox, if status has changed
+      if (checkbox.checked === (select.value === '99')) {
+        checkbox.click()
+      }
+    })
+  })
+  done()
+})
