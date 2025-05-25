e9093c5e9943938623d84aeffabecf120d3edbea
CuriosBasant <basantbrpl@gmail.com>
2025-05-25 22:19:35 +0530

    revert: ⏪️ "feat: ✨ integrate captcha recognition for login page"
    This reverts commit 80a28d7b94c2deee4512d3354b2b499133624098.

--- src/entrypoints/login.content.ts
+++ src/entrypoints/login.content.ts
@@ -1,5 +1,3 @@
-import { createWorker, PSM } from 'tesseract.js'
-
 export default defineContentScript({
   matches: [
     'https://rajshaladarpan.rajasthan.gov.in/*/OfficeLoginNew.aspx',
@@ -13,27 +11,5 @@ export default defineContentScript({
 
     // Remove annoying alert message on blur for empty captcha input
     captchaInput.removeAttribute('onblur')
-
-    const img = document.querySelector<HTMLImageElement>('#bodyContent_Img2')
-    if (img) {
-      const readCaptcha = async (img: HTMLImageElement) => {
-        captchaInput.value = ''
-        const { text } = await ocrImage(img)
-        captchaInput.value = text.trim().replace(/ +/g, '')
-      }
-      readCaptcha(img)
-      img.addEventListener('load', async (ev) => readCaptcha(ev.currentTarget as HTMLImageElement))
-    }
   },
 })
-
-async function ocrImage(img: HTMLImageElement) {
-  const worker = await createWorker('eng')
-  await worker.setParameters({
-    tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
-    tessedit_pageseg_mode: PSM.SINGLE_WORD,
-  })
-  const { data } = await worker.recognize(img)
-  await worker.terminate()
-  return data
-}
