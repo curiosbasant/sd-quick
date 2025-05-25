3f4a8b63a8de8211fd3ae540afec3f94635134b8
CuriosBasant <basantbrpl@gmail.com>
2025-05-25 21:32:04 +0530

    feat: ✨ add popup login for session expired blank pages

--- src/entrypoints/global.content.ts
+++ src/entrypoints/global.content.ts
@@ -3,17 +3,17 @@ export default defineContentScript({
   main() {
     setGlobalDefaults()
     setFavicon('/SD1/Home/Public2/assets/img/home-icon.png')
+    autoDetectTabTitle()
 
-    // Set the window title
-    document.title = (() => {
-      const title =
-        // Get the title from the active breadcrumb or from the page title
-        document.querySelector('ol.breadcrumb li.active, .box_heading h2')?.textContent?.trim() ??
-        // Get the last pathname segment from url
-        location.pathname.split('/').at(-1)?.slice(0, -5)
-
-      return title ? `${title} | ShalaDarpan` : 'ShalaDarpan'
-    })()
+    // add popup login for session expired blank pages
+    if (document.body.children.length === 0) {
+      const p = document.body.appendChild(document.createElement('p'))
+      p.append(
+        'Oops! Your session has expired unexpectedly, please ',
+        createButton({ children: 'Login', onClick: handlePopupLogin }),
+        ' again to continue!'
+      )
+    }
 
     // Prevent annoying login page always being opening in new tab
     document
@@ -60,4 +60,42 @@ function setGlobalDefaults() {
   )
   setFormElementValue(formElements, 'ctl00$ContentPlaceHolder1$ddlstream', '3') // Arts
   setFormElementValue(formElements, 'ctl00$ContentPlaceHolder1$ddlsubject', '0') // All
+}
+
+function autoDetectTabTitle() {
+  const title =
+    // Get the title from the active breadcrumb or from the page title
+    document.querySelector('ol.breadcrumb li.active, .box_heading h2')?.textContent?.trim() ??
+    // Get the last pathname segment from url
+    location.pathname.split('/').at(-1)?.slice(0, -5)
+
+  document.title = title ? `${title} | ShalaDarpan` : 'ShalaDarpan'
+}
+
+function handlePopupLogin() {
+  const popup = window.open(
+    `${location.pathname.slice(0, 4)}/Home/Public2/OfficeLoginNew.aspx`,
+    'popup-login',
+    'popup=1,toolbar=0,location=0,menubar=0,scrollbars=1,status=1,width=500,height=650'
+  )
+  if (!popup) return
+  popup.focus()
+  popup.addEventListener('DOMContentLoaded', () => {
+    popup.document.querySelector('main')?.scrollIntoView()
+  })
+  const frc = () => {
+    if (
+      popup.document.readyState === 'loading' &&
+      popup.location.pathname.toLowerCase().includes('school')
+    ) {
+      console.log('ho gya login')
+      cancelAnimationFrame(animationFrameId)
+      popup.close()
+      window.location.reload()
+      return
+    }
+
+    animationFrameId = requestAnimationFrame(frc)
+  }
+  let animationFrameId = requestAnimationFrame(frc)
 }
