fb731ceb7c163cba50ac0f79122f60478720010f
CuriosBasant <basantbrpl@gmail.com>
2025-05-26 22:01:12 +0530

    refactor: ⚗️ expose `makeSdRequest` to `window`

--- src/entrypoints/window.content.ts
+++ src/entrypoints/window.content.ts
@@ -1,5 +1,8 @@
+import { makeSdRequest } from '~/utils/request'
+
 declare global {
   interface Window {
+    makeSdRequest: typeof makeSdRequest
     timeInSecondsAfterSessionOut: number
     StartThisSessionTimer: () => void
   }
@@ -9,6 +12,8 @@ export default defineContentScript({
   matches: ['https://rajshaladarpan.rajasthan.gov.in/*'],
   world: 'MAIN',
   main() {
+    // Expose utility functions
+    window.makeSdRequest = makeSdRequest
     // Infinite session timeout
     window.timeInSecondsAfterSessionOut = Infinity
     // Optional, just to avoid creating unnecessary timeouts
